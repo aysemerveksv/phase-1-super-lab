20ac4905e153cbde8d8a4c5ae8b9cd9331a9a40b
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-05-26 21:10:54 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your classes here
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+
+  static definition() {
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+  }
+}
+
+class Deciduous extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`;
+  }
+}
+
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`;
+  }
+}
+
+// Testing the classes
+const oak = new Deciduous("Deciduous", "Oak");
+console.log(oak.species); // Deciduous
+console.log(oak.name); // Oak
+console.log(Deciduous.definition()); // A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Deciduous trees shed their leaves annually.
+
+const pine = new Evergreen("Evergreen", "Pine");
+console.log(pine.species); // Evergreen
+console.log(pine.name); // Pine
+console.log(Evergreen.definition()); // A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves. Evergreens keep their leaves all year round.
